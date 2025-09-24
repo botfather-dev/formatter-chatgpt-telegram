6dbb9adef4010109ac70647258b02232108213b2
latand <latand666@gmail.com>
2025-09-24 18:40:33 +0300

    Bump version to 0.3.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="chatgpt_md_converter",
-    version="0.3.8",
+    version="0.3.9",
     author="Kostiantyn Kriuchkov",
     author_email="latand666@gmail.com",
     description="A package for converting markdown to HTML for chat Telegram bots",
